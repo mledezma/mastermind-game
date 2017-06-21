ae7824120ca88fd305cd22af1655630a5336214b
Marco Ledezma <marcoledezmacordero09@gmail.com>
2017-06-21 17:55:56 -0600

    Added a new testing with command pattern

--- js/command.js
+++ js/command.js
@@ -38,7 +38,66 @@ function drop(event) {
     event.target.appendChild(document.getElementById(data));
     event.dataTransfer.clearData();
 }
-    
+
+// Events
 document.getElementById('color').addEventListener('dragstart', dragStart);
 document.getElementById('target').addEventListener('drop', drop);
 document.getElementById('target').addEventListener('dragover', dragover);
+document.getElementById('target2').addEventListener('drop', drop);
+document.getElementById('target2').addEventListener('dragover', dragover);
+
+
+
+/*-----------------------------------------------------
+---------------------Command Test----------------------
+-----------------------------------------------------*/
+
+
+function dragExecute(event, value){
+    console.log('event', event);  
+    console.log('value', value);  
+};
+var Command = function(execute,event,value) {
+    this.execute = execute;
+    this.event = event;
+    this.value = value;  
+    this.hi = 'hi';  
+};
+var DragCommand = function(event, value){
+    return new Command(dragExecute,event,value);
+};
+
+var Mastermind = function() {
+    var commands = [];
+
+    return {
+        execute: function(command) {
+            commands.push(command);
+            // command.value = target; command.name.function --> EventListener
+            var test = document.getElementById(command.value);
+            test.addEventListener('click', function(){
+                console.log(command.hi)
+            });
+            log.add(commands[commands.length-1]);  
+            return test;          
+        },
+        getCommands: function() {
+            return commands;
+        }
+    }
+};
+
+var config = {
+    value: 'test',
+    hi: 'Hello',
+}
+
+var run = function(){
+    var mastermind = new Mastermind();
+    var test2 = new DragCommand(event,'test');
+    console.log(test2);
+    mastermind.execute(test2);
+    
+};
+
+run();
