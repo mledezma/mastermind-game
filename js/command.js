e434b92d8ec1ca0cde7d373b9a7905d90d903508
Marco <marcoledezmacordero09@gmail.com>
2017-06-22 01:52:52 -0600

    Command Pattern
    Adapt the code to a command design pattern ::boom::

--- js/command.js
+++ js/command.js
@@ -11,12 +11,35 @@ var log = (function(){
     }
 })();
 
+// // Events
+// document.getElementById('target').addEventListener('drop', drop);
+// document.getElementById('target').addEventListener('dragover', dragover);
+// document.getElementById('target2').addEventListener('drop', drop);
+// document.getElementById('target2').addEventListener('dragover', dragover);
+
+
+
+/*-----------------------------------------------------
+---------------------Command Test----------------------
+-----------------------------------------------------*/
+
 /**
- * @function dragStart
+ * @function drag
  * @param {string} event
  */
-function dragStart(event) {
+function drag(event) {
     event.dataTransfer.setData("text/plain", event.target.id);
+}
+
+/**
+ * @function drop
+ * @param {string} event
+ */
+function drop(event) {
+    event.preventDefault();
+    var data = event.dataTransfer.getData("text");
+    event.target.appendChild(document.getElementById(data));
+    event.dataTransfer.clearData();
 }
 
 /**
@@ -28,58 +51,61 @@ function dragover(event) {
 }
 
 /**
- * @function Drop
- * @param {string} event
+ * @constructor
+ * @description Default class for Commands
+ * @param {function} execute - The callback to execute
+ * @param {string} name - The name of the event listener
+ * @param {DOMElement} target - DOM Element to aply the last param
  */
-function drop(event) {
-    console.log("Drop");
-    event.preventDefault();
-    var data = event.dataTransfer.getData("text");
-    event.target.appendChild(document.getElementById(data));
-    event.dataTransfer.clearData();
-}
-
-// Events
-document.getElementById('color').addEventListener('dragstart', dragStart);
-document.getElementById('target').addEventListener('drop', drop);
-document.getElementById('target').addEventListener('dragover', dragover);
-document.getElementById('target2').addEventListener('drop', drop);
-document.getElementById('target2').addEventListener('dragover', dragover);
-
-
-
-/*-----------------------------------------------------
----------------------Command Test----------------------
------------------------------------------------------*/
-
-
-function dragExecute(event, value){
-    console.log('event', event);  
-    console.log('value', value);  
-};
-var Command = function(execute,event,value) {
+var Command = function(execute, name, target) {
     this.execute = execute;
-    this.event = event;
-    this.value = value;  
-    this.hi = 'hi';  
-};
-var DragCommand = function(event, value){
-    return new Command(dragExecute,event,value);
+    this.name = name;    
+    this.target = target;
 };
 
+/**
+ * @instance Command#DragCommand
+ * @param {string} name - The name of the event listener
+ * @param {DOMElement} target - DOM Element to aply the last param
+ */
+var DragCommand = function(name, target){
+    return new Command(drag, name, target);
+};
+
+/**
+ * @instance Command#DropCommand
+ * @param {string} name - The name of the event listener
+ * @param {DOMElement} target - DOM Element to aply the last param
+ */
+var DropCommand = function(name, target){
+    return new Command(drop, name, target);
+};
+
+//ToDo: Try to fusion Dragover and Drop command because both have the same target
+/**
+ * @instance Command#DragoverCommand
+ * @param {string} name - The name of the event listener
+ * @param {DOMElement} target - DOM Element to aply the last param
+ */
+var DragoverCommand = function(name, target){
+    return new Command(dragover, name, target);
+};
+
+/**
+ * @constructor
+ */
 var Mastermind = function() {
     var commands = [];
 
     return {
         execute: function(command) {
-            commands.push(command);
+            commands.push(command.name);
             // command.value = target; command.name.function --> EventListener
-            var test = document.getElementById(command.value);
-            test.addEventListener('click', function(){
-                console.log(command.hi)
+            var input = document.getElementById(command.target);
+            input.addEventListener(command.name, function(event){
+                command.execute(event);
             });
-            log.add(commands[commands.length-1]);  
-            return test;          
+            log.add(commands[commands.length-1]);      
         },
         getCommands: function() {
             return commands;
@@ -87,17 +113,19 @@ var Mastermind = function() {
     }
 };
 
-var config = {
-    value: 'test',
-    hi: 'Hello',
-}
-
+/**
+ * @function run
+ */
 var run = function(){
     var mastermind = new Mastermind();
-    var test2 = new DragCommand(event,'test');
-    console.log(test2);
-    mastermind.execute(test2);
-    
-};
-
-run();
+    var drag = new DragCommand('dragstart', 'color');
+    var drop = new DropCommand('drop', 'target');
+    var dragover = new DragoverCommand('dragover', 'target');
+    console.log('drag', drag);
+    console.log('drop', drop);
+    console.log('dragover', dragover);
+    mastermind.execute(drag);
+    mastermind.execute(dragover);    
+    mastermind.execute(drop);
+    //log.show();
+}();
