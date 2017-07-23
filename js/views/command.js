414defa253c6a574c57f593f3f8d922fb7c02c2d
Marco Ledezma <marcoledezmacordero09@gmail.com>
2017-07-23 13:24:19 -0600

    Added a branch with part of the view
    Part of the view

--- js/views/command.js
+++ js/views/command.js
@@ -1,112 +1,10 @@
-var pattern = [];
-
-/**
- * @function Log Helper
- * @param {} msg
- */
-var log = (function() {
-    var log = " ";
-
-    return {
-        add: function(msg) { log += msg + "\n" },
-        show: function() { alert(log); log = ""}
-    }
-})();
-
-/**
- * @function drag
- * @param {string} event
- */
-function drag(event) {
-    event.dataTransfer.setData("text/plain", event.target.id);
-}
-
-/**
- * @function drop
- * @param {string} event
- */
-function drop(event) {
-    event.preventDefault();
-    var data = event.dataTransfer.getData("text");
-    var target = event.target;
-    target.appendChild(document.getElementById(data));
-    event.dataTransfer.clearData();
-
-    // Adds every new dropped element to the pattern array
-    pattern = [];
-    var targets = target.getElementsByTagName('li');
-    for(var i = 0; i < targets.length; i++) {
-        pattern.push(targets[i].id);
-    }
-<<<<<<< HEAD:js/views/command.js
-
-=======
->>>>>>> refs/remotes/origin/master:js/command.js
-    console.log(pattern);
-}
-
-/**
- * @function dragover
- * @param {string} event
- */
-function dragover(event) {
-    event.preventDefault();
-}
-
-/**
- * @constructor
- * @description Default class for Commands
- * @param {function} execute - The callback to execute
- * @param {string} name - The name of the event listener
- * @param {DOMElement} target - DOM Element to aply the last param
- */
-var Command = function(execute, name) {
-    this.execute = execute;
-    this.name = name;
-};
-
-/**
- * @instance Command#DragCommand
- * @param {string} name - The name of the event listener
- */
-var DragCommand = function(name) {
-    return new Command(drag, name);
-};
-
-/**
- * @instance Command#DropCommand
- * @param {string} name - The name of the event listener
- */
-var DropCommand = function(name) {
-    return new Command(drop, name);
-};
-
-/**
- * @constructor
- */
-var Mastermind = function() {
-    var commands = [];
-
-    return {
-        execute: function(command) {
-            commands.push(command);
-            log.add(commands[commands.length-1].name);
-            commands.forEach(function(element){
-                element.execute(event);
-                commands.pop();
-            });
-        },
-        getCommands: function() {
-            return commands;
-        }
-    }
-};
-
 /**
  * @function run
  */
+// This is mostly part of the controller
 var run = function() {
     // Variables
+    var pattern = [];
     var color = document.getElementById('red');
     var color2 = document.getElementById('blue');
     var color3 = document.getElementById('white');
@@ -119,12 +17,13 @@ var run = function() {
     var target = document.getElementById('target');
 
     // Instances
-    var mastermind = new Mastermind();
-    var drag = new DragCommand('dragstart');
-    var drop = new DropCommand('drop');
+    console.log(game_ui);
+    var mastermind = new game_ui.Mastermind();
+    var drag = new game_ui.DragCommand('dragstart');
+    var drop = new game_ui.DropCommand('drop');
 
     // Events
-    target.addEventListener('dragover', dragover);
+    target.addEventListener('dragover',  game_ui.dragover);
 
     color.addEventListener('dragstart', function(){
         mastermind.execute(drag);
@@ -154,6 +53,4 @@ var run = function() {
     target.addEventListener('drop', function(){
         mastermind.execute(drop);
     });
-
-    console.log(pattern);
 }();
--- js/views/view.js
+++ js/views/view.js
@@ -1,1 +1,96 @@
-// set events handlers to html elements
+/**
+ * @class 
+ * @desc Creates a view MVC class
+ */
+var game_ui = (function () {
+    /**
+     * @function drag
+     * @param {string} event
+     */
+    function drag(event) {
+        event.dataTransfer.setData("text/plain", event.target.id);
+    }
+
+    /**
+     * @function drop
+     * @param {string} event
+     */
+    function drop(event) {
+        event.preventDefault();
+        var data = event.dataTransfer.getData("text");
+        var target = event.target;
+        target.appendChild(document.getElementById(data));
+        event.dataTransfer.clearData();
+
+        // Adds every new dropped element to the pattern array
+        pattern = [];
+        var targets = target.getElementsByTagName('li');
+        for (var i = 0; i < targets.length; i++) {
+            pattern.push(targets[i].id);
+        }
+        console.log(pattern);
+    }
+
+    /**
+     * @function dragover
+     * @param {string} event
+     */
+    function dragover(event) {
+        event.preventDefault();
+    }
+
+    /**
+     * @constructor
+     * @description Default class for Commands
+     * @param {function} execute - The callback to execute
+     * @param {string} name - The name of the event listener
+     * @param {DOMElement} target - DOM Element to aply the last param
+     */
+    var Command = function (execute, name) {
+        this.execute = execute;
+        this.name = name;
+    };
+
+    /**
+     * @instance Command#DragCommand
+     * @param {string} name - The name of the event listener
+     */
+    var DragCommand = function (name) {
+        return new Command(drag, name);
+    };
+
+    /**
+     * @instance Command#DropCommand
+     * @param {string} name - The name of the event listener
+     */
+    var DropCommand = function (name) {
+        return new Command(drop, name);
+    };
+
+    /**
+     * @constructor
+     */
+    var Mastermind = function () {
+        var commands = [];
+
+        return {
+            execute: function (command) {
+                commands.push(command);
+                commands.forEach(function (element) {
+                    element.execute(event);
+                    commands.pop();
+                });
+            },
+            getCommands: function () {
+                return commands;
+            }
+        }
+    };
+
+    return {
+        Mastermind: Mastermind,
+        DragCommand: DragCommand,
+        DropCommand: DropCommand,
+        dragover: dragover,
+    }
+}())
